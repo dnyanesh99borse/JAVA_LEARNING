be5976de6a3a384fff6b01555b195a6e28e4b458
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2025-01-23 20:40:18 +0530

    Strings and Strings functions

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 
+import java.util.Scanner;
+import javax.print.attribute.SetOfIntegerSyntax;
+
+
 // import java.util.*;
 // import java.util.Arrays;
 // public class Main{
@@ -33,29 +37,114 @@
 //=========================2D ARRAYS======================================
 
 
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the number of rows of Array: ");
-    int rows = sc.nextInt();
-    System.out.print("Enter the number of cols of Array: ");
-    int cols = sc.nextInt();
+// import java.util.*;
+// public class Main{
+//     public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the number of rows of Array: ");
+//     int rows = sc.nextInt();
+//     System.out.print("Enter the number of cols of Array: ");
+//     int cols = sc.nextInt();
 
-    int[][] numbers = new int[rows][cols];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            System.out.print("Enter the Element [" + i + "][" + j + "] of Array: ");
-            numbers[i][j] = sc.nextInt();
-        }
-    }
+//     int[][] numbers = new int[rows][cols];
+//     for(int i = 0; i < rows; i++){
+//         for(int j = 0; j < cols; j++){
+//             System.out.print("Enter the Element [" + i + "][" + j + "] of Array: ");
+//             numbers[i][j] = sc.nextInt();
+//         }
+//     }
 
-    System.out.println("\nArray is:");
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            System.out.print( numbers[i][j] + " ");
-        }
-        System.out.println();
-    }
-  }
-}
+//     System.out.println("\nArray is:");
+//     for(int i = 0; i < rows; i++){
+//         for(int j = 0; j < cols; j++){
+//             System.out.print( numbers[i][j] + " ");
+//         }
+//         System.out.println();
+//     }
+//   }
+// }
+
+
+
+// import java.util.*;
+// public class Main{
+//     public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the number of rows of Array: ");
+//     int rows = sc.nextInt();
+//     System.out.print("Enter the number of cols of Array: ");
+//     int cols = sc.nextInt();
+
+//     int[][] numbers = new int[rows][cols];
+//     for(int i = 0; i < rows; i++){
+//         for(int j = 0; j < cols; j++){
+//             System.out.print("Enter the Element [" + i + "][" + j + "] of Array: ");
+//             numbers[i][j] = sc.nextInt();
+//         }
+//     }
+
+//     System.out.print("Enter the number to find: ");
+//     int x = sc.nextInt();
+//     System.out.println("\nArray is:");
+//     for(int i = 0; i < rows; i++){
+//         for(int j = 0; j < cols; j++){
+//             System.out.print( numbers[i][j] + " ");
+//             if(numbers[i][j] == x){
+//                 System.out.println("\nElement " + x + " is found at position [" + i +
+//                 "][" + j + "]");
+//             }
+//         }
+//         System.out.println();
+//     }
+//   }
+// }
+
+
+//===========================STRINGS======================================//
+// public class Main{
+//     public static void main(String args[]){
+//         String name = "Dnyaneshwar";
+//         String Fullname = "Dnyaneshwar Sambhaji Borse";
+//         String sentence = "Hi am a Successfull Independent Billionaire";
+
+//         System.out.println(name +Fullname + sentence);
+//     }
+// }
+
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter your name: ");
+//         String name = sc.nextLine();  // Use nextLine() to capture full name including spaces
+
+//         System.out.print("Enter your Bio: ");
+//         String bio = sc.nextLine();  // Use nextLine() to capture full bio including spaces
+
+//         System.out.println("The name of the user is: " + name);
+//         System.out.println("The Bio of the user is: " + bio);
+//     }
+// }
+
+//DIFFERENT FUNCTIONS AND METHODS ON STRING:
+
+//1).compareTo() = to compare two different strings.
+// public class Main{
+//     public static void main(String[] args) {
+//         String name1 = "Dnyanesh";
+//         String name2 = "Elon";
+
+        
+//         if(name1.compareTo(name2) == 0){
+//             System.out.println(name1 + " equal to " + name2);
+//         }else if(name1.compareTo(name2) < 0){
+//             System.out.println(name1 + " is less than " + name2);
+//         }else if(name1.compareTo(name2) > 0){
+//             System.out.println(name1 + " is greater than " + name2);
+//         }
+//     }
+// }
+
+
